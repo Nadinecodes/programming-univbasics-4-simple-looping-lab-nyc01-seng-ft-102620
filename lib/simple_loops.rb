d91526e08606ad6bcf49c2bbf342505c23e5e26f
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-09-29 20:10:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -25,8 +25,8 @@ end
 
 def return_string_array(array)
   count = 0
+  new_array=[]
   while count<array.length do
-    new_array=[]
     new_array.push(array[count].to_s)
     count+=1
     return new_array
