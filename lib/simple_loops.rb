62c3223eeaed4a89f6084bdb4c87925d6edc1a28
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-09-29 20:02:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,28 @@
-# Write your methods here
+def loop_message_five_times(message)
+  count=0 
+  while count<6 do
+    puts message
+    count+=1
+  end
+end  
+
+def loop_message_n_times(message, n) 
+  n=0 
+  while n<message.length do
+    puts message
+    n+=1
+  end  
+
+end
+
+def output_array(array) 
+  n=0 
+  while array[n] do
+    puts array
+    n+=1
+  end
+end
+
+
+  
+  
